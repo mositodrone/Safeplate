5e56b3d0d71e18dd6f35d1a2b7aa3259b58c8cba
Dev Air <mositoprime@gmail.com>
2026-02-17 04:56:46 +0100

    fix database connection error

--- src/lib/database/mongoose.ts
+++ src/lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'Safeplate', bufferCommands: false 
+      bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
