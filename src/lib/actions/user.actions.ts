cc8137c6b567d941b98301f7a7fb7a6428609644
Dev Air <mositoprime@gmail.com>
2026-02-17 04:31:05 +0100

    Fix createUser discrepancy

--- src/lib/actions/user.actions.ts
+++ src/lib/actions/user.actions.ts
@@ -11,11 +11,18 @@ export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
 
+    const existingUser = await User.findOne({ clerkId: user.clerkId });
+
+    if (existingUser) {
+      return JSON.parse(JSON.stringify(existingUser));
+    }
+
     const newUser = await User.create(user);
 
     return JSON.parse(JSON.stringify(newUser));
   } catch (error) {
-    handleError(error);
+    console.error("CREATE USER ERROR:", error);
+    throw error;
   }
 }
 
